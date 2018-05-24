646a21fbd88f49e1c03d1a3069518245fc6fe990
sandraortiz <rocio12vx@gmail.com>
2018-05-24 09:22:07 -0500

    empezando encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,26 @@
-window.cipher = {
-  // ... 
-};
+//window.cipher = {
+
+  const mensajeInput = document.getElementById('mensaje');
+const buttonEncriptar = document.getElementById('button-encriptar');
+const claveInput = document.getElementById("clave");
+
+const encriptar = () => {
+  const clavesita= parseInt(claveInput.value);
+ const mensajito = mensajeInput.value;
+  
+for(let i = 0; i < mensajito.length; i++){
+  var c = mensajito.charCodeAt(i);
+ var final = "";
+    var e = c + clavesita;
+  final += String.fromCharCode(e);
+  console.log(final);
+
+document.getElementById("final").innerHTML = final;
+}
+}
+
+
+
+buttonEncriptar.addEventListener('click', encriptar);
+
+
