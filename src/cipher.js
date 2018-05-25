7ad6f0106b7450f293eb9a8caf26b828ef93a073
sandraortiz <rocio12vx@gmail.com>
2018-05-25 09:27:31 -0500

    funcion decode , estilos y html

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,36 @@
 
+const menssage1Input = document.getElementById('menssage1');
+const menssage2Input = document.getElementById('menssage2');
+const buttonEncode = document.getElementById('button-encode');
+const buttonDecode= document.getElementById('button-decode');
+const keyInput = document.getElementById("key");
 
-const mensajeInput = document.getElementById('mensaje');
-const buttonEncriptar = document.getElementById('button-encriptar');
-const claveInput = document.getElementById("clave");
-
-
-const encriptar = () => {
-  const clavesita= parseInt(claveInput.value);
-  const mensajito = mensajeInput.value.toUpperCase();
-    let final = "";
-  for(let i = 0; i < mensajito.length; i++){
-      const c = (mensajito.charCodeAt(i)-65+clavesita)%26+65;
-       final += String.fromCharCode(c);
+const encode = () => {
+  const Keys= parseInt(keyInput.value);
+  const messageE = menssage1Input.value.toUpperCase();
+    let messageEncode = "";
+  for(let i = 0; i < messageE.length; i++){
+      const c = (messageE.charCodeAt(i)-65+Keys)%26+65;
+      messageEncode += String.fromCharCode(c);
       
 }
-  document.getElementById("final").innerHTML= final;
+  document.getElementById("encodemessage").innerHTML= messageEncode;
 }  
 
-buttonEncriptar.addEventListener('click', encriptar);
+buttonEncode.addEventListener('click', encode);
 
+
+
+const decode = () => {
+  const Keys= parseInt(keyInput.value);
+  const messageD = menssage2Input.value.toUpperCase();
+  let messageDecode = "";
+  for(let i = 0; i < messageD.length; i++){
+      const b = (messageD.charCodeAt(i)-65-Keys+26)%26+65;
+      messageDecode += String.fromCharCode(b);
+      
+}
+  document.getElementById("decodemessage").innerHTML= messageDecode;
+}  
+
+buttonDecode.addEventListener('click', decode);
