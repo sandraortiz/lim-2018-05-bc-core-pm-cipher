91325ccfedae25898f2f35a782124015a40f28b1
sandraortiz <rocio12vx@gmail.com>
2018-06-04 10:15:28 -0500

    añadiendo estilos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const menssagedecode = document.getElementById('menssage-decode');
 const buttonDecode = document.getElementById('button-decode');
 const buttonEncode = document.getElementById('button-encode');
 const encodeDecodemessage  = document.getElementById('encodedecode-message');
+const buttonWelcome = document.getElementById("button-welcome");
+const mysecretPage=document.getElementById("inicio");
+const welcomePage=document.getElementById("welcomepage");
 
 buttonEncode.addEventListener('click', ()=>{
     let Key = keyInput.value;
@@ -16,4 +19,9 @@ buttonDecode.addEventListener ("click" , ()=>{
     let Key = keyInput.value;
     let menssageDe = menssagedecode.value;
     encodeDecodemessage.value = window.cipher.decode(Key,menssageDe); 
+})
+
+buttonWelcome.addEventListener('click', ()=>{
+    mysecretPage.style.display="block";
+    welcomePage.style.display="none";
 })
